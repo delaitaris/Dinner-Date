b45b3def05d6a8a3b770643fe355411373a11d7f
delaitaris <tomdawarrior@gmail.com>
2026-03-31 10:21:24 +1300

    added basic stuff

--- scenes/dinner_date.gd
+++ scenes/dinner_date.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Dialogic.start("starter")
+	if Dialogic.current_timeline != null:
+		pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,8 +13,4 @@ func _process(delta: float) -> void:
 	pass
 
 func _input(event: InputEvent) -> void:
-	if Dialogic.current_timeline != null:
-		pass
-	if event is InputEvent and event.is_pressed():
-		Dialogic.start("starter")
-		
+	pass
