05879fc5a2cd80ea3ece9a2db762fdb7407158bc
delaitaris <tomdawarrior@gmail.com>
2026-04-02 10:14:27 +1300

    added a proper beginning speeech
    might be corny but i'll rework it sometime idk

--- scenes/button.gd
+++ scenes/button.gd
@@ -1,12 +1,10 @@
-extends Node2D
+extends Button
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-#calls dialogic to begin at start of opening
-	Dialogic.start("beginning speech")
-	if Dialogic.current_timeline != null:
-		pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
