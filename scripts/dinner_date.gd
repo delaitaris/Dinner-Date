9d68f206f35dc467cfe49c375d5f6cdf02dae5db
delaitaris <tomdawarrior@gmail.com>
2026-04-22 14:29:54 +1200

    added half a prologue and placeholders for background

--- scripts/dinner_date.gd
+++ scripts/dinner_date.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 #calls dialogic to begin at start of opening
-	Dialogic.start("beginning speech")
+	Dialogic.start("prologue")
 	if Dialogic.current_timeline != null:
 		pass
 
