fcccfdf4bb05b1303e52dc0e08b0e8d289c9ff39
Bvnak <132238328+lunaf2016@users.noreply.github.com>
2026-05-28 12:43:41 +1200

    Kitchen scene done placeholder stuff

---
+++ scripts/kitchen.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_chips_button_pressed() -> void:
+	Dialogic.start("beerbatteredchips")
+	get_tree().change_scene_to_file("res://scenes/talking.tscn")
+	if Dialogic.current_timeline != null:
+		pass
+
+
+
+
+func _on_wings_button_pressed() -> void:
+	Dialogic.start("buffalowings")
+	get_tree().change_scene_to_file("res://scenes/talking.tscn")
+	if Dialogic.current_timeline != null:
+		pass
