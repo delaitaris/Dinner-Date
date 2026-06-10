51d3839b67c4586f5b0ba55a0795663807fc470d
delaitaris <tomdawarrior@gmail.com>
2026-06-10 14:19:14 +1200

    added the transfer state between name screen and selection

--- scripts/dinner_date.gd
+++ scripts/dinner_date.gd
@@ -3,13 +3,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 #calls dialogic to begin at start of opening
 	Dialogic.start("prologue")
 	if Dialogic.current_timeline != null:
 		pass
 
+
+func _on_dialogic_signal(argument:String):
+	if argument == "selection":
+		get_tree().change_scene_to_file("res://scenes/kitchen.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Dialogic.timeline_ended("prologue"):
-		print("okay")
-		
+	pass
