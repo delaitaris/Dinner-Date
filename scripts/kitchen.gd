3db702dede12e111797236597304fbd1e77dc301
Bvnak <132238328+lunaf2016@users.noreply.github.com>
2026-06-22 09:55:09 +1200

    Transition to scene 2

--- scripts/kitchen.gd
+++ scripts/kitchen.gd
@@ -8,13 +8,13 @@ func _ready() -> void:
 
 func _on_chips_button_pressed() -> void:
 	Dialogic.start("beerbatteredchips")
-	get_tree().change_scene_to_file("res://scenes/talking.tscn")
+	get_tree().change_scene_to_file("res://scenes/kitchen_2.tscn")
 	if Dialogic.current_timeline != null:
 		pass
 
 
 func _on_wings_button_pressed() -> void:
 	Dialogic.start("buffalowings")
-	get_tree().change_scene_to_file("res://scenes/talking.tscn")
+	get_tree().change_scene_to_file("res://scenes/kitchen_2.tscn")
 	if Dialogic.current_timeline != null:
 		pass
