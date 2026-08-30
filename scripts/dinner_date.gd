dcbbf19544315ad943708e098712ef5e94ca70df
Bvnak <132238328+lunaf2016@users.noreply.github.com>
2026-08-30 22:04:27 +1200

    Halfway done with a Rewind dialogue mechanic

--- scripts/dinner_date.gd
+++ scripts/dinner_date.gd
@@ -1,30 +1,27 @@
 extends Node2D
 
+var block_dialogic_click := false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 	Dialogic.timeline_ended.connect(_on_dialogic_timeline_ended)
 
-	# calls dialogic to begin at start of opening
 	Dialogic.start("prologue")
-	
-
-	if Dialogic.current_timeline != null:
-		pass
 
 
 func _on_dialogic_signal(argument: String):
+	if argument == "save_state":
+		RewindSystem.save_state()
+
 	if argument == "selection":
 		get_tree().change_scene_to_file("res://scenes/kitchen.tscn")
 
 
-# Called when the Dialogic timeline finishes
 func _on_dialogic_timeline_ended():
 	await Transition.fade_to_black(2.0)
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if block_dialogic_click:
+		get_viewport().set_input_as_handled()
